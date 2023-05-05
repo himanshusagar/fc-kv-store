7054fe04682885b65ba0265aacf9cb41caf64b22
Leigh B Stoller <stoller@flux.utah.edu>
2023-05-04 20:44:25 -0500

    reorganized. Added a bunch of code mostly for Get(). Added protos

--- src/customer.cc
+++ src/customer.cc
@@ -1,72 +1,117 @@
 #include "fc_kv_store.grpc.pb.h"
+#include <string>
+#include <vector>
+#include "customer.h"
 
-#include <grpcpp/ext/proto_server_reflection_plugin.h>
-#include <grpcpp/grpcpp.h>
-#include <signal.h>
-
-using grpc::Channel;
-using grpc::ClientContext;
-using grpc::Status;
-
-using fc_kv_store::FCKVStoreRPC;
 using fc_kv_store::GetRequest;
 using fc_kv_store::GetResponse;
 using fc_kv_store::PutRequest;
 using fc_kv_store::PutResponse;
+using fc_kv_store::StartOpRequest;
+using fc_kv_store::StartOpResponse;
+using fc_kv_store::CommitOpRequest;
+using fc_kv_store::CommitOpResponse;
 
-class FCKVStoreRPCClient
-{
-public:
-  FCKVStoreRPCClient(std::shared_ptr<Channel> channel)
-      : stub_(FCKVStoreRPC::NewStub(channel)) {}
+std::string FCKVClient::Get(std::string key) {
+  std::vector<VersionStruct> all_versions;
+  StartOp(&all_versions);
 
-  int FCKVStoreGet(int in)
-  {
-    GetRequest req;
-    req.set_key(in);
+  // update local version
+  // increment version number
+  VersionStruct new_version = version_;
+  new_version.set_version(version_.get_version() + 1);
 
-    GetResponse reply;
-    ClientContext context;
-    Status status = stub_->FCKVStoreGet(&context, req, &reply);
+  // update version vector
+  for (auto v : all_versions) {
+    (*new_version.mutable_vlist)[v.get_pubkey()] = v.get_version();
+  }
+  // don't forget about ourselves
+  (*new_version.mutable_vlist)[pubkey_] = new_version.get_version();
+  all_versions.push_back(new_version);
 
-    if (status.ok())
-    {
-      return reply.value();
-    }
-    else
-    {
-      std::cout << status.error_code() << ": " << status.error_message()
-                << std::endl;
-      return -1;
-    }
+  // if there's a history conflict, we abort now
+  if (!CheckCompatability(all_versions)) {
+    std::cout << "History incompatability, aborting operation" << std::cout;
+    AbortOp();
   }
 
-  int FCKVStorePut(int key, int value)
-  {
-    PutRequest req;
-    req.set_key(key);
-    req.set_value(value);
+  // Do GetRequest with H(value) from key table
+  GetRequest req;
+  std::string hashvalue = (*new_version.mutable_itable())[key];
+  req.set_key(hashvalue);
+  
+  GetResponse reply;
+  ClientContext context;
+  Status status = stub_->FCKVStoreGet(&context, req, &reply);
+  
+  if (status.ok() && CommitOp(new_version) == Status::Ok()) {
+    version_ = new_version;
+    return reply.value();
+  }
+  
+  std::cout << status.error_code() << ": " << status.error_message()
+            << std::endl;
+  return nullptr;
+}
+
+int FCKVClient::Put(std::string key, std::string value) {
+  PutRequest req;
+  req.set_key(key);
+  req.set_value(value);
     
+  PutResponse reply;
+  ClientContext context;
+  Status status = stub_->FCKVStorePut(&context, req, &reply);
 
-    PutResponse reply;
-    ClientContext context;
-    Status status = stub_->FCKVStorePut(&context, req, &reply);
+  if (status.ok())
+  {
+    return reply.status();
+  }
+  else
+  {
+    std::cout << status.error_code() << ": " << status.error_message()
+              << std::endl;
+    return -1;
+  }
+}
 
-    if (status.ok())
-    {
-      return reply.status();
-    }
-    else
-    {
-      std::cout << status.error_code() << ": " << status.error_message()
-                << std::endl;
-      return -1;
+// we should expect the server to return the complete list of version structs
+// TODO optimization - only return the diff between what we and the server know
+Status FCKVClient::StartOp(std::vector<VersionStruct>* versions) {
+  StartOpRequest req;
+  StartOpResponse reply;
+  ClientContext context;
+  Status status = stub_->FCKVStoreStartOp(&context, req, &reply);
+  if (status.ok()) {
+    // update local version lists
+    for (VersionStruct v : req->versions()) {
+      versions->push_back(v);
     }
   }
+  return status;
+}
 
+Status FCKVClient::CommitOp(VersionStruct version) {
+  CommitOpRequest req;
+  CommitOpResponse reply;
+  ClientContext context;
+
+  req.set_version(version);
+  return stub_->FCKVStoreCommitOp(&context, req, &reply);
+}
 private:
   std::unique_ptr<FCKVStoreRPC::Stub> stub_;
+  std::unique_ptr<VersionStruct> version_;
+  std::vector<std::unique_ptr<VersionStruct>> versions_;
 };
+
+Status FCKVClient::AbortOp() {
+  return Status::Ok();
+}
+
+bool FCKVClient::CheckCompatability(std::vector<VersionStruct> versions) {
+  return True;
+}
 
 void sigintHandler(int sig_num)
 {
@@ -84,13 +129,14 @@ int main()
   signal(SIGINT, sigintHandler);
 
   const std::string target_str = "localhost:50051";
-  FCKVStoreRPCClient rpcClient(
-      grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()));
+  FCKVClient rpcClient(
+    grpc::CreateChannel(target_str, grpc::InsecureChannelCredentials()),
+    "12345");
   int user(96);
-
-  int placedValue = 20;
-  rpcClient.FCKVStorePut(100 , placedValue);
-  auto reply = rpcClient.FCKVStoreGet(100);
+  
+  std::string placedValue = "20";
+  rpcClient.Put("100" , placedValue);
+  auto reply = rpcClient.Get("100");
   std::cout << "BasicRPC Int received: " << reply << " " << placedValue << std::endl;
 
   return 0;
---
+++ src/customer.h
@@ -0,0 +1,47 @@
+#include "fc_kv_store.grpc.pb.h"
+
+#include <grpcpp/ext/proto_server_reflection_plugin.h>
+#include <grpcpp/grpcpp.h>
+#include <signal.h>
+#include <vector>
+#include <string>
+
+using grpc::Channel;
+using grpc::ClientContext;
+using grpc::Status;
+
+using fc_kv_store::FCKVStoreRPC;
+using fc_kv_store::VersionStruct;
+
+class FCKVClient
+{
+public:
+  FCKVClient(std::shared_ptr<Channel> channel, std::string pubkey)
+    : stub_(FCKVStoreRPC::NewStub(channel)),
+      pubkey_(pubkey) {}
+  
+  std::string Get(string key);
+  int Put(string key, string value);
+  
+private:
+  std::unique_ptr<FCKVStoreRPC::Stub> stub_;  // gRPC
+  VersionStruct version_;                     // local version struct
+  std::vector<VersionStruct> versions_;       // global version structs
+  std::string pubkey_;                        // this is us
+  
+  // acquires global lock on server
+  // updates local copies of version lists as returned by the server
+  //               do we need to rollback if we abort this operation??
+  Status StartOp(std::vector<VersionStruct>* versions);
+  
+  // releases global lock on server
+  // sends updated version struct to server
+  Status CommitOp(VersionStruct version);
+    
+  // aborts the operation - releases the lock, do not send version struct
+  Status AbortOp();
+  
+  // If a list of version structs is not totally ordered by >=, there is a
+  // history conflict
+  bool CheckCompatability(std::vector<VersionStruct> versions);
+};
