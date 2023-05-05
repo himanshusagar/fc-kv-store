5756fc8c2975bc19b64b2e05057154332d387260
owlesia <leself40@gmail.com>
2023-05-04 23:01:26 -0500

    client can generate keys with OpenSSL and sign VersionStruct

--- src/customer.cc
+++ src/customer.cc
@@ -1,6 +1,12 @@
 #include "fc_kv_store.grpc.pb.h"
+
+#include <filesystem>
 #include <string>
 #include <vector>
+#include <iostream>
+#include <openssl/rsa.h>
+#include <openssl/pem.h>
+
 #include "customer.h"
 
 using fc_kv_store::GetRequest;
@@ -11,6 +17,52 @@ using fc_kv_store::StartOpRequest;
 using fc_kv_store::StartOpResponse;
 using fc_kv_store::CommitOpRequest;
 using fc_kv_store::CommitOpResponse;
+
+std::string privateKeyFile = "private_key.pem";
+std::string publicKeyFile = "public_key.pem";
+
+bool signVersionStruct(VersionStruct& versionStruct) {
+    // Serialize the VersionStruct without the signature field
+    versionStruct.clear_signature();
+    std::string serializedData = versionStruct.SerializeAsString();
+
+    // Load the private key from the file
+    FILE* privateKeyFilePtr = fopen(privateKeyFile.c_str(), "rb");
+    if (!privateKeyFilePtr) {
+        std::cerr << "Failed to open private key file." << std::endl;
+        return false;
+    }
+
+    RSA* privateKey = PEM_read_RSAPrivateKey(privateKeyFilePtr, nullptr, nullptr, nullptr);
+    fclose(privateKeyFilePtr);
+    if (!privateKey) {
+        std::cerr << "Failed to read private key from file." << std::endl;
+        return false;
+    }
+
+    // Calculate the hash of the serialized data
+    unsigned char hash[SHA256_DIGEST_LENGTH];
+    SHA256_CTX sha256;
+    SHA256_Init(&sha256);
+    SHA256_Update(&sha256, serializedData.c_str(), serializedData.size());
+    SHA256_Final(hash, &sha256);
+
+    // Sign the hash using the private key
+    unsigned int signatureLen = RSA_size(privateKey);
+    std::vector<unsigned char> signature(signatureLen);
+    int result = RSA_sign(NID_sha256, hash, SHA256_DIGEST_LENGTH, signature.data(), &signatureLen, privateKey);
+    RSA_free(privateKey);
+
+    if (result != 1) {
+        std::cerr << "Failed to sign the VersionStruct content." << std::endl;
+        return false;
+    }
+
+    // Set the signature field in the VersionStruct
+    versionStruct.set_signature(signature.data(), signature.size());
+
+    return true;
+}
 
 std::string FCKVClient::Get(std::string key) {
   std::vector<VersionStruct> all_versions;
@@ -28,6 +80,11 @@ std::string FCKVClient::Get(std::string key) {
   // don't forget about ourselves
   (*new_version.mutable_vlist())[pubkey_] = new_version.version();
   all_versions.push_back(new_version);
+
+  // sign new version struct
+  if (!signVersionStruct(new_version)) {
+      std::cerr << "Failed to sign the VersionStruct content." << std::endl;
+  }
 
   // if there's a history conflict, we abort now
   if (!CheckCompatability(all_versions)) {
@@ -123,10 +180,73 @@ void sigintHandler(int sig_num)
   std::exit(0);
 }
 
+bool generateRSAKeyPair() {
+    int keyLength = 2048;
+    int exp = RSA_F4; // Standard RSA exponent: 65537
+
+    // Generate RSA key
+    RSA* rsa = RSA_generate_key(keyLength, exp, nullptr, nullptr);
+    if (!rsa) {
+        std::cerr << "Failed to generate RSA key pair." << std::endl;
+        return false;
+    }
+
+    // Write private key to file
+    FILE* privateKeyFilePtr = fopen(privateKeyFile.c_str(), "wb");
+    if (!privateKeyFilePtr) {
+        std::cerr << "Failed to open private key file." << std::endl;
+        RSA_free(rsa);
+        return false;
+    }
+
+    if (!PEM_write_RSAPrivateKey(privateKeyFilePtr, rsa, nullptr, nullptr, 0, nullptr, nullptr)) {
+        std::cerr << "Failed to write private key to file." << std::endl;
+        fclose(privateKeyFilePtr);
+        RSA_free(rsa);
+        return false;
+    }
+
+    fclose(privateKeyFilePtr);
+
+    // Write public key to file
+    FILE* publicKeyFilePtr = fopen(publicKeyFile.c_str(), "wb");
+    if (!publicKeyFilePtr) {
+        std::cerr << "Failed to open public key file." << std::endl;
+        RSA_free(rsa);
+        return false;
+    }
+
+    if (!PEM_write_RSAPublicKey(publicKeyFilePtr, rsa)) {
+        std::cerr << "Failed to write public key to file." << std::endl;
+        fclose(publicKeyFilePtr);
+        RSA_free(rsa);
+        return false;
+    }
+
+    fclose(publicKeyFilePtr);
+    RSA_free(rsa);
+
+    return true;
+}
+
 int main()
 {
   // "ctrl-C handler"
   signal(SIGINT, sigintHandler);
+
+  // --START-- Generate private and public key
+  if (std::filesystem::exists(privateKeyFile) || std::filesystem::exists(publicKeyFile)) {
+      std::cerr << "Private or public key file already exists. Please remove or rename them before generating a new key pair." << std::endl;
+      return 1;
+  }
+
+  if (generateRSAKeyPair()) {
+      std::cout << "RSA key pair generated successfully." << std::endl;
+  } else {
+      std::cerr << "Failed to generate RSA key pair." << std::endl;
+      return 1;
+  }
+  // --END-- Generate private and public key
 
   const std::string target_str = "localhost:50051";
   FCKVClient rpcClient(
